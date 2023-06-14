1e8119feb3048068f59c5244a96ea6f111da3295
dimdimcraft <dimdimcraft.id@gmail.com>
2023-06-14 11:32:46 +0700

    tugas crud tipeMobil

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,18 @@ Route::get('/mobil',[MobilController::class,'index']);
 
 Route::get('/mobil/create',[MobilController::class,'create']); //memangil form
 Route::post('/mobil/simpanData',[MobilController::class,'store']); //mengimpan data
-Route::get('/merk',[MerkController::class, 'index']);
 
+Route::get('/merk',[MerkController::class, 'index']);
 Route::get('/merk/create',[MerkController::class,'create']);
 Route::post('/merk/simpanData',[MerkController::class, 'store']);
-
 Route::get('/merk/edit/{id}',[MerkController::class, 'edit']);
 Route::post('/merk/update/{id}', [MerkController::class, 'update']);
 Route::get('/merk/delete/{id}',[MerkController::class, 'delete']);
+
+// Route Folder Tipe Mobil
+Route::get('/tipe_mobil', [TipeMobilController::class, 'index']);
+Route::get('/tipe_mobil/create', [TipeMobilController::class, 'create']);
+Route::post('/tipe_mobil/simpan-data', [TipeMobilController::class, 'store']);
+Route::get('/tipe_mobil/edit/{id}', [TipeMobilController::class, 'edit']); //edit data
+Route::post('/tipe_mobil/update/{id}', [TipeMobilController::class, 'update']);
+Route::get('/tipe_mobil/delete/{id}', [TipeMobilController::class, 'delete']);
